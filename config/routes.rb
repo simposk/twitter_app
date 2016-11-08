22d4d82eaca55d325f76ba0cd2c42df11525be69
simposk <simposk@gmail.com>
2016-11-08 17:47:22 +0200

    Fix navigation, change root_path based if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'timeline#index'
+  get 'static_pages/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  authenticated :user do
+    root 'timeline#index'
+  end
+
+  root "static_pages#index"
 end
